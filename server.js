6131d6505963da565d78d4e47dd7c6fcb166567f
Pham Hoang Kha <khahoang123tv@gmail.com>
2025-03-28 11:28:44 +0700

    Bai20

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
-const { createServer } = require('node:http');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; // Port number
+const path = require('path');
+const hostname = process.env.HOST_NAME; // Hostname
+require('dotenv').config()
+// check env
+console.log('Environment:', process.env);
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// Khai báo routeroute
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+/* app.get('/', (req, res) => {
+    res.render('index','./src/views/sample.ejs')
+  }) */
+  app.get('/', (req, res) => {
+    res.render('index', __dirname + '/src/views/sample.ejs')    
+  })
+/* Khai báoo routeroute
+app.get('/abcabc', (req, res) => {
+    res.send('Hello World!')
+  })
+*/
+// config template engine
+app.set('views', './src/views')   
+app.set('view engine', 'ejs')
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World :))');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port,() => {
+  console.log(`Example app listening on port ${port}`)
+})
